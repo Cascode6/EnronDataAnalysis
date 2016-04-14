e2ebc7d2c22c86b3355b45cc2934a0e44bf2ce69
Cascode6 <caseylfaist@gmail.com>
2016-04-13 22:04:10 -0500

    Second Draft
    Cleaned up poi_id and updated plk files to match

--- poi_id.py
+++ poi_id.py
@@ -20,24 +20,24 @@ import numpy as np
 ### The first feature must be "poi".
 features_list = ['poi',
                  'salary', 
-                 'bonus',
-                 'restricted_stock',
-                 'restricted_stock_deferred',
-                 'total_stock_value',
-                 'shared_receipt_with_poi',
-                 'exercised_stock_options',
-                 'to_messages',
-                 'from_messages',
-                 'loan_advances',
+                 'deferral_payments', 
+                 'total_payments', 
+                 'loan_advances', 
+                 'bonus', 
+                 'restricted_stock_deferred', 
+                 'deferred_income', 
+                 'total_stock_value', 
+                 'expenses', 
+                 'exercised_stock_options', 
+                 'other', 
+                 'long_term_incentive', 
+                 'restricted_stock', 
                  'director_fees',
-                 'expenses',
-                 'other',
-                 'total_payments',
-                 'deferral_payments',
-                 'long_term_incentive',
-                 'from_this_person_to_poi',
-                 'from_poi_to_this_person',
-                 'deferred_income',
+                 'to_messages',
+                 'from_poi_to_this_person', 
+                 'from_messages', 
+                 'from_this_person_to_poi', 
+                 'shared_receipt_with_poi'
                  ]
 
 
@@ -78,16 +78,7 @@ features_list.append("from_emails_poi_ratio")
 features_list.append("to_emails_poi_ratio")
 
 
-######### eventually, create text-based count features #################
-
 ### Store to my_dataset for easy export below.
-
-# n_people = 0
-# for person in data_dict:
-#     n_people = n_people + 1
-# print "Number of people in dataset: ", n_people
-# print "Number of features used: ", len(features_list)
-# print "Total number of datapoints: ", n_people * len(features_list)
 removed_features = []
 nan_percents = {} #to compare against feature predictive power later
 for feature in features_list:
@@ -98,11 +89,11 @@ for feature in features_list:
         if (feature, "NaN") in data_dict[person].items():
             total_nan = total_nan + 1
     nan_percents[feature] = float(total_nan) / float(total)
-    if nan_percents[feature] >= 0.75:
+    if nan_percents[feature] > 0.4:
         removed_features.append(feature)
         features_list.remove(feature)
-# print removed_features
-# print features_list
+print removed_features
+print features_list
 
 my_dataset = data_dict
 
@@ -118,7 +109,7 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 
-####visualization function
+#### for visualizing the actual and predicted outcomes
 def visualizeData(features_test, labels_test, prediction):
     counter = 0
     for point in features_test: 
@@ -158,23 +149,10 @@ def visualizeData(features_test, labels_test, prediction):
     return None
 
 
-
-
-
-
-#allows for a comparison between percent of NaN values and the importance to the classifier
+#allows for a comparison between feature Nan ratio and classifier importance
 def get_Importances(classifier, filter):
     print "************************************"
-    
-    # imps = classifier.feature_importances_
     counter = 0
-    # truecount = 0
-    # for i in filter.get_support():
-    #     feature_name = features_list[counter]
-    #     if i == True:
-    #         print feature_name, "NaN:", nan_percents[feature_name], "Imp:", imps[truecount]
-    #         truecount +=1
-    #     counter += 1
     for i in filter.get_support():
         if i == True:
             print features_list[counter]
@@ -185,10 +163,6 @@ def get_Importances(classifier, filter):
 # Provided to give you a starting point. Try a variety of classifiers.
 # from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.cluster import KMeans
-
-# dt = DecisionTreeClassifier()
-
 
 clf = AdaBoostClassifier(algorithm = "SAMME.R", 
                          learning_rate = 1.99)
@@ -207,7 +181,10 @@ clf = AdaBoostClassifier(algorithm = "SAMME.R",
 
 # #taken from tester.py
 from sklearn.cross_validation import StratifiedShuffleSplit
-cv = StratifiedShuffleSplit(labels, n_iter=100, test_size=0.05, random_state = 42)
+cv = StratifiedShuffleSplit(labels, 
+                            n_iter=100, 
+                            test_size=0.05,
+                            random_state = 42)
 for train_idx, test_idx in cv: 
         features_train = []
         features_test  = []
@@ -219,87 +196,32 @@ for train_idx, test_idx in cv:
         for jj in test_idx:
             features_test.append( features[jj] )
             labels_test.append( labels[jj] )
-# print "Number of train features:", len(features_train)
-# print "Number of train POIs:", sum(labels_train)
-# print "Number of test features:", len(features_test)
-# print "Number of test POIs:", sum(labels_test)
+print "Total observations:", len(labels)
+print "Total target observations:", sum(labels)
+print "Total features:", len(features)
+print "Number of train features:", len(features_train)
+print "Number of train POIs:", sum(labels_train)
+print "Number of test features:", len(features_test)
+print "Number of test POIs:", sum(labels_test)
 
 
-
-# from sklearn.feature_selection import SelectFromModel
+from sklearn.feature_selection import SelectFromModel
 from sklearn.pipeline import Pipeline
 from sklearn.decomposition import PCA
 
+sfm = SelectFromModel(clf)
+sfm.fit_transform(features_train, labels_train)
 
-# ###Decision Tree Classifier Only
-# sfm = SelectFromModel(dt)
-# sfm.fit(features_train, labels_train)
-# Feature_transform = sfm.transform(features_train)
-# PipeSelect = Pipeline([("select_from_model", sfm), ("dtc", dt)])
-# t0 = time()
-# PipeSelect.fit(features_train, labels_train)
-# print "Decision Tree training time:", round(time() - t0, 3), "s"
-# prediction = PipeSelect.predict(features_test)
-# print "Decision Tree Score:", PipeSelect.score(features_test, labels_test)
-# #check the results
-# visualizeData(features_test, labels_test, prediction)
-# get_Importances(dt, sfm)
-
-
-# sfm = SelectFromModel(clf)
-# sfm.fit(features_train, labels_train)
-
-
-
-#from PCA lesson:
-def doPCA(n_components):
-    pca = PCA(n_components = n_components)
-    pca.fit(data)
-    return pca
-
-n_components = 2
-pca = doPCA(n_components)
-print pca.explained_variance_ratio_
-first_pca = pca.components_[0]
-second_pca = pca.components_[1]
-# third_pca = pca.components_[2]
-
-transformed_data = pca.transform(data)
-
-
-
-
-# #AdaBoostClassifier and SelectFromModel
-# PipeSelect = Pipeline([("select_from_model", sfm), ("adaboost", clf)])
-# t0 = time()
-# PipeSelect.fit(features_train, labels_train)
-# print "SFM training time:", round(time() - t0, 3), "s"
-# prediction = PipeSelect.predict(features_test)
-# print "SFM Score:", PipeSelect.score(features_test, labels_test)
-# #check the results
-# visualizeData(features_test, labels_test, prediction)
-# get_Importances(clf, sfm)
-
-print "n pca features:", n_components
-PipeSelect = Pipeline([("pca", pca), ("adaboost", clf)])
+#AdaBoostClassifier and SelectFromModel
+PipeSelect = Pipeline([("select_from_model", sfm), ("adaboost", clf)])
 t0 = time()
 PipeSelect.fit(features_train, labels_train)
-print "pca training time:", round(time() - t0, 3), "s"
+print "SFM training time:", round(time() - t0, 3), "s"
 prediction = PipeSelect.predict(features_test)
-print "pca Score:", PipeSelect.score(features_test, labels_test)
-
+print "SFM Score:", PipeSelect.score(features_test, labels_test)
 #check the results
-for ii, jj in zip(transformed_data, data):
-    plt.scatter(first_pca[0]*ii[0], first_pca[1]*ii[0], color="r")
-    plt.scatter(second_pca[0]*ii[0], second_pca[1]*ii[0], color="c")
-    # plt.scatter(third_pca[0]*ii[0], third_pca[1]*ii[0])
-    plt.scatter(jj[0], jj[1], color="b")
-    
-plt.show()
-
 visualizeData(features_test, labels_test, prediction)
-
-
+get_Importances(clf, sfm)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
